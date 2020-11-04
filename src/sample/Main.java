0171ce606d8d8777b4a4ce01d1c82f62369cc423
Antoineuuh <73739424+Antoineuuh@users.noreply.github.com>
2020-11-04 09:29:34 +0100

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,23 +14,6 @@ import sql.DbManager;
 
 import java.io.File;
 
-/**
- * This file is part of Aster.
- *
- * Aster is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Aster is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Aster. If not, see <http://www.gnu.org/licenses/>.
- */
-
 public class Main extends Application {
 
     //@../../../../Desktop/avatar/null1.png
@@ -66,7 +49,8 @@ public class Main extends Application {
         Thread THREAD = new Consumer("TO_OTHER");
         THREAD.start();
 
-        System.out.println(Uploader.UPLOAD(new File("C:/Users/Antoine/Desktop/MONTAGE FORTNITE/MINIATURE_MONTAGE_FORTNITE.png")));
+        //Test pour l´integration avec Imgur.
+        //System.out.println(Uploader.UPLOAD(new File("C:/Users/Antoine/Desktop/MONTAGE FORTNITE/MINIATURE_MONTAGE_FORTNITE.png")));
 
         launch(args);
 
